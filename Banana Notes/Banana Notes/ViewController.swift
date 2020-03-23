756e5dc013df629eb8aad916f4c4dab8949b20b6
Shah Samiur Rahman <sam@Shahs-MacBook-Pro.local>
2020-03-22 22:20:07 -0700

    Initial Rendering of the front-end of user authentication functionality.

---
+++ Banana Notes/Banana Notes/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Banana Notes
+//
+//  Created by Shah Samiur Rahman on 3/21/20.
+//  Copyright © 2020 Shah Samiur Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var bLoginButton: UIButton!
+    
+    @IBOutlet weak var gLoginButton: UIButton!
+    @IBOutlet weak var fLoginButton: UIButton!
+    
+    @IBOutlet weak var bSignUpLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
